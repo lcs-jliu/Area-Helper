92fef07e12e8dbc34a40153ce24d333a757ba1d7
Jason Liu <jliu@lcs.on.ca>
2019-10-16 20:08:00 -0400

    Initial Commit

---
+++ Area Helper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Area Helper
+//
+//  Created by Liu, Jason on 2019-10-16.
+//  Copyright © 2019 Liu Jason. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
