519148ae340418a6bf5c6388b1b2730404ce1f66
Jason Liu <jliu@lcs.on.ca>
2019-10-23 16:09:22 -0400

    Connected the screens with view controllers

---
+++ Area Helper/ParallelogramViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Area Helper
+//
+//  Created by Liu, Jason on 2019-10-16.
+//  Copyright © 2019 Liu Jason. All rights reserved.
+//
+
+import UIKit
+
+class ParallelogramViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
